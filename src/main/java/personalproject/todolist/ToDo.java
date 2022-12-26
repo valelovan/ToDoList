69790ad09710464efed4c86e8a4d726f021de9b4
Vale Lovan <vrl5chk@virginia.edu>
2022-12-25 22:23:41 -0500

    Added fields for ToDo objects. Added Gradle import for Java Persistence API.

--- src/main/java/personalproject/todolist/ToDo.java
+++ src/main/java/personalproject/todolist/ToDo.java
@@ -1,7 +1,12 @@
 package personalproject.todolist;
 
+import java.util.Date;
+
+
 public class ToDo {
     private int id;
     private String title;
     private String description;
+    private Date createDate;
+    private Date dueDate;
 }
