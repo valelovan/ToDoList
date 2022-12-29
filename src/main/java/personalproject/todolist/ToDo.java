11e1624b9061ccddd6efec5217271a0bf088483f
Vale Lovan <vrl5chk@virginia.edu>
2022-12-29 15:28:33 -0500

    Implemented ToDo POJO.

--- src/main/java/personalproject/todolist/ToDo.java
+++ src/main/java/personalproject/todolist/ToDo.java
@@ -2,11 +2,70 @@ package personalproject.todolist;
 
 import java.util.Date;
 
-
 public class ToDo {
     private int id;
     private String title;
     private String description;
-    private Date createDate;
-    private Date dueDate;
+    private Date dateCreated;
+    private Date dateDue;
+
+    /**
+     * ID field getter.
+     * @return ToDo's ID.
+     */
+    public int getId() {return id;}
+
+    /**
+     * ID field setter.
+     * @param id New ID.
+     */
+    public void setId(int id) {this.id = id;}
+
+    /**
+     * Title field getter.
+     * @return ToDo's Title.
+     */
+    public String getTitle() {return title;}
+
+    /**
+     * Title field setter.
+     * @param title New Title.
+     */
+    public void setString(String title) {this.title = title;}
+
+    /**
+     * Description field getter.
+     * @return ToDo's Description.
+     */
+     public String getDescription() {return description;}
+
+    /**
+     * Description field setter.
+     * @param description New Description.
+     */
+    public void setDescription(String description) {this.description = description;}
+
+    /**
+     * DateCreated field getter.
+     * @return ToDo's DataCreated
+     */
+    public Date getDateCreated() {return dateCreated;}
+
+    /**
+     * DateCreated field setter.
+     * @param dateCreated New DateCreated.
+     */
+    public void setDateCreated(Date dateCreated) {this.dateCreated = dateCreated;}
+
+    /**
+     * DateDue field getter.
+     * @return ToDo's DateCreated.
+     */
+    public Date getDateDue() {return dateDue;}
+
+    /**
+     * DateDue field setter.
+     * @param dateDue New DateDue.
+     */
+    public void setDateDue(Date dateDue) {this.dateDue = dateDue;}
 }
