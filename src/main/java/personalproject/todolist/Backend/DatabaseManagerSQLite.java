8adf5bb480d38cb592eeff96b2732c9d02c1d76f
Vale Lovan <vrl5chk@virginia.edu>
2022-12-29 16:33:48 -0500

    Added method stubs from DatabaseManagerInterface to DatabaseManagerSQLite.

--- src/main/java/personalproject/todolist/Backend/DatabaseManagerSQLite.java
+++ src/main/java/personalproject/todolist/Backend/DatabaseManagerSQLite.java
@@ -1,23 +1,71 @@
 package personalproject.todolist.Backend;
 
+import personalproject.todolist.ToDo;
+
 import java.io.Closeable;
+import java.util.List;
 
 public class DatabaseManagerSQLite implements DatabaseManagerInterface, Closeable {
 
+
+    @Override
     public void connect() {
-        //
+
     }
 
+    @Override
     public void createTables() {
-        //
+
     }
 
+    @Override
     public void clearTables() {
-        //
+
     }
 
+    @Override
     public void deleteTables() {
-        //
+
+    }
+
+    @Override
+    public void insertGroup(String groupName) {
+
+    }
+
+    @Override
+    public void deleteGroup(String groupName) {
+
+    }
+
+    @Override
+    public boolean groupExists(String groupName) {
+        return false;
+    }
+
+    @Override
+    public void insertToDo(ToDo todo) {
+
+    }
+
+    @Override
+    public List<ToDo> selectToDos(String todoTitle) {
+        return null;
+    }
+
+    @Override
+    public void deleteToDo(int id) {
+
+    }
+
+    @Override
+    public List<ToDo> selectAllTodos() {
+        return null;
+    }
+
+    @Override
+    public List<ToDo> selectAllTodos(String groupTitle) {
+        return null;
     }
 
     public void close() {
