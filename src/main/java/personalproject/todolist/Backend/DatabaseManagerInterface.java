ef33acee3f66763d608f9da352fbaa53d1daee51
Vale Lovan <vrl5chk@virginia.edu>
2022-12-29 16:12:08 -0500

    Added methods to DatabaseManagerInterface to allow storing and retrieving of ToDo objects and groups.

--- src/main/java/personalproject/todolist/Backend/DatabaseManagerInterface.java
+++ src/main/java/personalproject/todolist/Backend/DatabaseManagerInterface.java
@@ -1,6 +1,9 @@
 package personalproject.todolist.Backend;
 
+import personalproject.todolist.ToDo;
+
 import java.io.Closeable;
+import java.util.List;
 
 public interface DatabaseManagerInterface {
     /**
@@ -22,4 +25,49 @@ public interface DatabaseManagerInterface {
      * Drops the tables in the database.
      */
     public void deleteTables();
+
+    /**
+     * Adds a new group to the ToDo group table.
+     * @param groupName Name of the new ToDo group.
+     */
+    public void addGroup(String groupName);
+
+    /**
+     * Removes a group from the ToDo group table.
+     * @param groupName Name of the ToDo group.
+     */
+    public void removeGroup(String groupName);
+
+    /**
+     * Check if there is a group with the given title in the database.
+     * @param groupName Title of the ToDo group.
+     * @return True if there is a group with the given title, false otherwise.
+     */
+    public boolean groupExists(String groupName);
+
+    /**
+     * Inserts a ToDo object into the database.
+     * @param todo ToDo to be inserted.
+     */
+    public void insertToDo(ToDo todo);
+
+    /**
+     * Selects ToDo from tables given ToDo title.
+     * @param todoTitle Title of the ToDo.
+     * @return ToDo object with the provided title.
+     */
+    public ToDo selectToDo(String todoTitle);
+
+    /**
+     * Selects all ToDos from the table and returns them in a list.
+     * @return List of all ToDos.
+     */
+    public List<ToDo> selectAllTodos();
+
+    /**
+     * Selects all ToDos with the given group title from the table.
+     * @param groupTitle Title of the ToDo group.
+     * @return List of all ToDos from the group.
+     */
+    public List<ToDo> selectAllTodosFromGroup(String groupTitle);
 }
