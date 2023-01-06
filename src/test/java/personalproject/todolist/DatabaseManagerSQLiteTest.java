7f0d2c3d879ba39d3c266d258bd8f78746463792
Vale Lovan <vrl5chk@virginia.edu>
2023-01-06 16:16:29 -0500

    Added comments to section off groups of related tests.

--- src/test/java/personalproject/todolist/DatabaseManagerSQLiteTest.java
+++ src/test/java/personalproject/todolist/DatabaseManagerSQLiteTest.java
@@ -1,7 +1,51 @@
 package personalproject.todolist;
 
+import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 public class DatabaseManagerSQLiteTest {
-    //
+
+    // TESTS: public void close();
+
+    // TESTS: public void connect();
+
+
+    // TESTS: public void createTables();
+
+
+    // TESTS: public void clearTables();
+
+
+    // TESTS: public void deleteTables();
+
+
+    // TESTS: public void insertGroup(String groupName);
+
+
+    // TESTS: public void deleteGroup(String groupName);
+
+
+    // TESTS: public List<String> selectAllGroups();
+
+
+    // TESTS: public boolean groupExists(String groupName);
+
+
+    // TESTS: public void insertToDo(ToDo todo);
+
+
+    // TESTS: public List<ToDo> selectToDos(String todoTitle);
+
+
+    // TESTS: public void deleteToDo(int id);
+
+
+    // TESTS: public List<ToDo> selectAllTodos();
+
+
+    // TESTS: public List<ToDo> selectAllTodos(String groupTitle);
+
+
+    // TESTS: public List<ToDo> selectAllIncompleteToDos();
 }
