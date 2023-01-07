fd0737dcfdad80b507dbf20cdc0ffaa040090c70
Vale Lovan <vrl5chk@virginia.edu>
2023-01-06 19:21:50 -0500

    Added class to store the group info.

---
+++ src/main/java/personalproject/todolist/Group.java
@@ -0,0 +1,20 @@
+package personalproject.todolist;
+
+public class Group {
+    private int id;
+    private String name;
+    private String description;
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return description;}
+
+    public void setDescription() {this.description = description;}
+
+}
